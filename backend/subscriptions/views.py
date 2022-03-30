276661c558087d4e8ad4d5ede316dd2e73c436cb
Sergey <ssprihodko@yandex.ru>
2022-03-30 19:46:02 +0300

    deploy3

--- backend/subscriptions/views.py
+++ backend/subscriptions/views.py
@@ -1,5 +1,4 @@
 from rest_framework import mixins, viewsets
-from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticated
 
 from recipes.models import User
